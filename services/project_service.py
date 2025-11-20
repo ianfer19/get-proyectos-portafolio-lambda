216eddf2872ce8fddcce4bf002efc5ffb36c9c57
ianfer19 <ifernandezalvarez@correo.unicordoba.edu.co>
2025-11-20 01:32:05 -0500

    Refactorizar consulta de proyectos por habilidad para usar GSI y simplificar el código

--- services/project_service.py
+++ services/project_service.py
@@ -10,7 +10,18 @@ class ProjectService:
 
     @staticmethod
     def get_projects_by_skill(skill_id: str):
-        return ProjectRepository.query_by_skill(skill_id)
+        # Items del índice GSI
+        gsi_items = ProjectRepository.query_by_skill(skill_id)
+
+        projects = []
+        
+        for gsi_item in gsi_items:
+            project_id = gsi_item["projectId"]
+            full_project = ProjectRepository.get_by_id(project_id)
+            if full_project:
+                projects.append(full_project)
+
+        return projects
 
     @staticmethod
     def get_project_by_id(project_id: str):
